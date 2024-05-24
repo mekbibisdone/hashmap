5722df97b3b4d535b68ec9723df0f30875785b7f
mekbibisdone <mekbibtare@gmail.com>
2024-05-24 19:11:09 +0300

    Add the hash function

--- hashmap.js
+++ hashmap.js
@@ -1,3 +1,27 @@
-export function sayHello(){
-  return "hello world"
+export default function HashMap(){
+  /*
+    Creates a hash map
+    @returns {object} - An object containing a hash function
+  */
+  let bucketLength = 16
+
+  function hash(key) {
+    /*
+    @param {string} key - The key to be hashed
+    @returns {number} - The hash code
+    */
+   if (key.length == 0){
+    throw Error("Key must have length greater than 0")
+   }
+    let hashCode = 0;
+       
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % bucketLength;
+    }
+ 
+    return hashCode;
+  } 
+  
+  return {hash}
 }
--- hashmap.test.js
+++ hashmap.test.js
@@ -1,6 +1,28 @@
-import { it,expect } from "vitest";
-import { sayHello } from "./hashmap";
+import { it,expect, describe } from "vitest";
+import HashMap from "./hashmap";
 
-it("Returns hello world string",() => {
-    expect(sayHello()).toEqual("hello world")
+describe("Hash", () =>{
+    const {hash} = HashMap();
+
+    it("throws an error if the string passed has length zero", () => {
+        const key = "";
+        expect(() => hash(key)).toThrow("Key must have length greater than 0")
+    })
+    it("returns the correct hashcode for passed string with length one",()=>{
+        const key = "a";
+        const hashCode = hash(key); 
+        expect(hashCode).toBe(1)
+    })
+    
+    it("returns the correct hashcode for a passed string with length two",() => {
+        const key = "ab";
+        const hashCode = hash(key); 
+        expect(hashCode).toBe(1)
+    })
+
+    it("returns the correct hashcode for a passed string with length five",() => {
+        const key = "abcde";
+        const hashCode = hash(key);
+        expect(hashCode).toBe(3)
+    })
 })
